92b5ed430a611d71cf33ccc98c5d86d02cedce57
Shraddha Bhujbal <shraddhabhu888@gmail.com>
2026-09-15 11:56:24 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (26.26%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int n=prices.size();
         int ans=0;
         int minprice=prices[0];
-        for(int i=0;i<n;i++)
+        for(int i=1;i<n;i++)
         {
             minprice=min(prices[i],minprice);
             ans=max(prices[i]-minprice,ans);
