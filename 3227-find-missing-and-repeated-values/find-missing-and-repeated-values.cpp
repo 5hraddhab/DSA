f7c16035e6d2bac13702e0182fc7235c9d13c319
Shraddha Bhujbal <shraddhabhu888@gmail.com>
2026-09-18 14:29:36 +0530

    Time: 0 ms (100.00%) | Memory: 25.5 MB (91.95%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,34 +1,62 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-          long long num = grid.size();
-          long long n=num*num;
-        long long actualSum = 0;
-        long long actualSquareSum = 0;
 
-       for(int i=0;i<num;i++)
-       {
-        for(int j=0;j<num;j++)
-        {
-            actualSum+=grid[i][j];
-            actualSquareSum+=(grid[i][j]*grid[i][j]);
+        int n = grid.size();
+        int total = n * n;
+
+        int xr = 0;
+
+        // XOR all grid elements
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                xr ^= grid[i][j];
+            }
         }
-       }
 
-        long long expectedSum = n * (n + 1) / 2;
-        long long expectedSquareSum = n * (n + 1) * (2 * n + 1) / 6;
+        // XOR numbers from 1 to n²
+        for (int i = 1; i <= total; i++) {
+            xr ^= i;
+        }
 
-        long long diff1 = actualSum - expectedSum;
-        long long diff2 = actualSquareSum - expectedSquareSum;
+        // Rightmost set bit
+        int bit = xr & (-xr);
 
-        // R - M = diff1
-        // R + M = diff2 / diff1
+        int a = 0;
+        int b = 0;
 
-        long long sum = diff2 / diff1;
+        // Divide grid elements into two groups
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
 
-        long long repeated = (diff1 + sum) / 2;
-        long long missing = repeated - diff1;
+                if (grid[i][j] & bit)
+                    a ^= grid[i][j];
+                else
+                    b ^= grid[i][j];
+            }
+        }
 
-        return {(int)repeated, (int)missing};
+        // Divide 1 to n² into two groups
+        for (int i = 1; i <= total; i++) {
+
+            if (i & bit)
+                a ^= i;
+            else
+                b ^= i;
+        }
+
+        // Determine which is repeated and which is missing
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (grid[i][j] == a)
+                    return {a, b};
+
+                if (grid[i][j] == b)
+                    return {b, a};
+            }
+        }
+
+        return {};
     }
 };
